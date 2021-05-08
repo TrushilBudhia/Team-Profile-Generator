cc1d102ef342059ba9e7e3f6be4e91224cc3e7d9
Trushil Budhia <trushil.budhia@gmail.com>
2021-05-08 21:52:12 +0800

    Updated Employee.js and Employee.test.js - ran 8 test and passed 8 test. Email input is also being validated

--- Employee.js
+++ Employee.js
@@ -1,6 +1,17 @@
 class Employee {
     constructor(id, name, email) {
-        throw Error('Please provide a valid id, name and email');
+        console.log(id, name, email);
+        const undefinedInput = id === undefined && name === undefined && email === undefined;
+        const validIdInput = Number.isInteger(id) && id > 0;
+        const validName = typeof name === 'string' && name.length > 0;
+
+        const validator = require('email-validator');
+        const verifyEmail = validator.validate(email);
+
+        const validEmail = typeof email === 'string' && email.length > 0 && verifyEmail === true;
+        if(undefinedInput || !validIdInput || !validName || !validEmail) {
+            throw Error('Please provide a valid id, name and email');
+        }
     }
 }
 
--- Employee.test.js
+++ Employee.test.js
@@ -1,9 +1,60 @@
 const Employee = require('./Employee.js');
 describe(`Employee`, () => {
+    // TEST 1 - no properties provided
     it(`An error is thrown if no properties are provided`, () => {
         const result = () => {
             new Employee(undefined, undefined, undefined);
         }
         expect(result).toThrow('Please provide a valid id, name and email');
     })
+    // TEST 2 - id not an integer
+    it(`An error is thrown if id is not an integer`, () => {
+        const result = () => {
+            new Employee('Bond', undefined, undefined);
+        }
+        expect(result).toThrow('Please provide a valid id, name and email');
+    })
+    // TEST 3 - id not a positive number
+    it(`An error is thrown if id is not a positive number`, () => {
+        const result = () => {
+            new Employee(-5, undefined, undefined);
+        }
+        expect(result).toThrow('Please provide a valid id, name and email');
+    })
+    // TEST 4 - name not a string
+    it(`An error is thrown if name is not a string`, () => {
+        const result = () => {
+            new Employee(1, 404, undefined);
+        }
+        expect(result).toThrow('Please provide a valid id, name and email');
+    })
+    // TEST 5 - name not an empty string
+    it(`An error is thrown if name is an empty string`, () => {
+        const result = () => {
+            new Employee(1, '', undefined);
+        }
+        expect(result).toThrow('Please provide a valid id, name and email');
+    })
+    // TEST 6 - email not a string
+    it(`An error is thrown if email is not a string`, () => {
+        const result = () => {
+            new Employee(7, 'James Bond', .007);
+        }
+        expect(result).toThrow('Please provide a valid id, name and email');
+    })
+    // TEST 7 - email not an empty string
+    it(`An error is thrown if email is an empty string`, () => {
+        const result = () => {
+            new Employee(7, 'James Bond', '');
+        }
+        expect(result).toThrow('Please provide a valid id, name and email');
+    })
+    // TEST 8 - email not a valid email address
+    it(`An error is thrown if email is not a valid email input`, () => {
+        const result = () => {
+            new Employee(7, 'James Bond', 'licenseToEmail');
+        }
+        expect(result).toThrow('Please provide a valid id, name and email');
+    })
+
 })
